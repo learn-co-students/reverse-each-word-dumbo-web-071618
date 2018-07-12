883788dd53f73020ab57ae8cfd2362fbbb6c022e
Kevin  Rivera <krivera1015@gmail.com>
2018-07-12 03:53:25 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
-  sentence.split.collect do |word|
-    word.reverse
-  end.join(" ")
+  sentence.split.collect { |word| word.reverse }
+  .join(" ")
 end
 
 #command backslash comment
