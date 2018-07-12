b9be26fcdab7cd010446967eb6b6178bbab15345
Kevin  Rivera <krivera1015@gmail.com>
2018-07-12 03:51:26 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,21 @@
+def reverse_each_word(sentence)
+  sentence.split.collect do |word|
+    word.reverse
+  end.join(" ")
+end
+
+#command backslash comment
+
+#.split will turn string to array
+#.join brings back to string and takes argument on what will go in between
+
+# require "pry"
+#
+# def reverse_each_word(sentence)
+#   new_sentence = []
+#   sentence.split.each do |word|
+#     new_sentence << word.reverse
+#   end
+#   new_sentence.join(" ")
+#   binding.pry
+# end
